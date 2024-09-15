18c20e7d80136a2321ed29855baf5be8a9b540ff
mk1018 <ktmn1018@yahoo.co.jp>
2024-09-15 10:47:08 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use openai_api_rs::v1::api::OpenAIClient;
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::common::GPT4_O;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = OpenAIClient::new(env::var("OPENAI_API_KEY").unwrap().to_string());
+
+    let req = ChatCompletionRequest::new(
+        GPT4_O.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: chat_completion::Content::Text(String::from("What is bitcoin?")),
+            name: None,
+            tool_calls: None,
+            tool_call_id: None,
+        }],
+    );
+
+    let result = client.chat_completion(req).await?;
+    println!("Content: {:?}", result.choices[0].message.content);
+    println!("Response Headers: {:?}", result.headers);
+
+    Ok(())
+}
