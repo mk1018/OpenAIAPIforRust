03e455aab28ca47cb10323810e88058b3bc0439a
mk1018 <ktmn1018@yahoo.co.jp>
2024-09-15 11:15:26 +0900

    微修正

---
+++ src/infrastructure/mod.rs
@@ -0,0 +1,3 @@
+pub mod openai {
+    pub mod openai_sample_request_repository;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-use openai_api_rs::v1::api::OpenAIClient;
-use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
-use openai_api_rs::v1::common::GPT4_O;
-use std::env;
+mod infrastructure;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = OpenAIClient::new(env::var("OPENAI_API_KEY").unwrap().to_string());
-
-    let req = ChatCompletionRequest::new(
-        GPT4_O.to_string(),
-        vec![chat_completion::ChatCompletionMessage {
-            role: chat_completion::MessageRole::user,
-            content: chat_completion::Content::Text(String::from("What is bitcoin?")),
-            name: None,
-            tool_calls: None,
-            tool_call_id: None,
-        }],
-    );
-
-    let result = client.chat_completion(req).await?;
-    println!("Content: {:?}", result.choices[0].message.content);
-    println!("Response Headers: {:?}", result.headers);
-
+    let response = infrastructure::openai::openai_sample_request_repository::execute().await?;
+    println!("Response content: {}", response);
     Ok(())
 }
